cca99f30902f9810f7ab752bfb28885623b01fd5
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2024-04-01 00:13:32 +1100

    Run formatter

--- src/features/snippets/jsSnippets/jsSnippetsUtils.js
+++ src/features/snippets/jsSnippets/jsSnippetsUtils.js
@@ -3,7 +3,7 @@ export function changeKeyBind(newKey, oldKey, shouldMap) {
         const callbacks = Spicetify.Mousetrap.trigger()._directMap
         const fromKey = shouldMap ? oldKey : newKey
         const toKey = shouldMap ? newKey : oldKey
-        const callbackName = Object.keys(callbacks).find(key => key.startsWith(fromKey))
+        const callbackName = Object.keys(callbacks).find((key) => key.startsWith(fromKey))
 
         Spicetify.Mousetrap.bind(toKey, callbacks[callbackName])
         Spicetify.Mousetrap.unbind(fromKey)
